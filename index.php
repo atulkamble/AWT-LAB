9b4a903347a9b9c1ec7f27cb687612e4bfc25b96
Atul Kamble <atul_kamble@hotmail.com>
2022-04-26 07:54:31 +0530

    Paassing Variables

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+ $fname="Dr. APJ Abdul";
+ $lname="Kalam";
+?>
+<a href="retrive.php?first_name=<?php echo $fname; ?>&last_name=<?php echo $lname; ?>">
+Click here to Pass Variable through URL
+</a>
---
+++ retrive.php
@@ -0,0 +1,4 @@
+<?php
+ echo "First Name : ".$_GET['first_name']."<br>";
+ echo "Last Name : ".$_GET['last_name'];
+?>
