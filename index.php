04fd3d6760f7a3fa45c25aad95ee47e81ccfd757
Atul Kamble <atul_kamble@hotmail.com>
2022-05-11 14:11:49 +0530

    Exception Handling

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-$handle = fopen("counter.txt", "r");
-if(!$handle)
-{ 
- echo "could not open the file" ;
+//create function with an exception
+function checkNum($number) {
+  if($number>1) {
+    throw new Exception("Value must be 1 or below");
+  }
+  return true;
 }
-else
-{
- $counter = (int ) fread($handle,20);
- fclose ($handle);
- $counter++;
- echo" <strong> you are visitor no ". $counter . " </strong> " ;
-        $handle = fopen("counter.txt", "w" );
-        fwrite($handle,$counter);
-        fclose($handle);
+
+//trigger exception in a "try" block
+try {
+  checkNum(2);
+  //If the exception is thrown, this text will not be shown
+  echo 'If you see this, the number is 1 or below';
+}
+
+//catch exception
+catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
 }
 ?>
